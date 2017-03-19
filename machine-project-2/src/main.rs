75f3999989cff6ed26f43311b82c6ceedc90d1c9
Jordan Crane <jordancrane@users.noreply.github.com>
2017-03-19 12:46:41 -0700

    Make timeout output more verbose

--- machine-project-2/src/main.rs
+++ machine-project-2/src/main.rs
@@ -23,13 +23,12 @@ fn main() {
     // Fetch config
     let (host, peers) = config::fetch_cli_options();
 
-    // Make random timeout between 150ms and 300ms
-    // let timeout = rand::thread_rng().gen_range(150, 301);
+    // Make random timeout between 300ms and 600ms
     let timeout = rand::thread_rng().gen_range(300, 600);
-    println!("{}", timeout);
-
+    
     // Start server
     println!("Creating server on {}", host);
+    println!("Randomized timeout: {}ms", timeout);
     let mut node = Node::new(host, timeout);
 
     // Leader timeout loop
